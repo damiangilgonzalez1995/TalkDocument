8d9ed8a7d1a88cfbaeb16cffaa54cff50a9a15a0
damiangilgonzalez1995 <damianbro1995@gmail.com>
2023-08-10 14:37:49 +0200

    Update add features

--- utils.py
+++ utils.py
@@ -1,2 +1,6 @@
 DS_TYPE_LIST = ["WEB", "PDF", "FILE"]
 SPLIT_TYPE_LIST = ["CHARACTER", "TOKEN"]
+EMBEDDING_TYPE_LIST = ["HF", "OPENAI"]
+VECTORSTORE_TYPE_LIST = ["FAISS", "CHROMA", "SVM"]
+REPO_ID_DEFAULT = "declare-lab/flan-alpaca-large"
+CHAIN_TYPE_LIST = ["stuff", "map_reduce", "map_rerank", "refine"]
